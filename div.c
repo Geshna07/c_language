9cfc59959c03b9d1ca34d7fd25df0a9aa41f5514
geshna <geshnab1507gmail.com>
2025-06-28 09:34:47 -0700

    division of two numbers

---
+++ div.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int num_1=4;
+	int num_2=2;
+	int num=num_1%num_2;
+	printf("the remainder of two numbers:%f",num);
+	float num1=num_1/num_2;
+	printf("the quotient of two numbers:%f",num1);
+	return 0;
+}
